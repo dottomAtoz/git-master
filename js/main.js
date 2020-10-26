c5fa6317f63b68c11d7f2ebab65cb79d7cac19b1
Tommy Alhamra <tommy.alhamra@gmail.com>
2020-10-26 15:47:51 +0700

    try router in web - problem

--- js/main.js
+++ js/main.js
@@ -57,24 +57,70 @@ Vue.component('alert', {
    <b>{{bold}}</b>
    {{msg}}</div>`,
 })
-// Vue.component('main-component', {
-//    props: ['num'],
-//    template: `   
-//       <article class="column content">                  
-//          ${{info[{{num}}].title}}
-//       </article>`,
-//    data() {
-//       return {
 
-//       }
-//    },
-// })
+const Home = {
+   template: `
+   <section id="main">
+      <nav>
+         Support me on :
+         <div class="nav-item">
+            <a href="https://www.behance.net/dottomstyleart" target="_blank">
+               <ion-icon name="logo-behance"></ion-icon>
+               <span>Behance</span>
+            </a>
+         </div>
+         <div class="nav-item">
+            <a href="https://medium.com/@tommyalhamra" target="_blank">
+               <ion-icon name="logo-medium"></ion-icon>
+               <span>Medium</span>
+            </a>
+         </div>
+         <div class="nav-item">
+            <a href="https://github.com/imotD" target="_blank">
+               <ion-icon name="logo-github"></ion-icon>
+               <span>Github</span>
+            </a>
+         </div>
+      </nav>
+      <main>
+         <div class="brand">
+            <img src="img/logo_git-master.png" alt="brand logo">
+         </div>
+      </main>
+      <div class="btn">
+         <router-link to="/guide">Guide</router-link>
+      </div>
+   </section>
+   `
+}
+
+const Guide = {
+   template: '<div>Guide Page</div>'
+}
+
+const routes = [{
+      path: '/',
+      component: Home
+   },
+   {
+      path: '/guide',
+      component: Guide
+   }
+
+]
+
+const router = new VueRouter({
+   mode: 'history',
+   routes // short for `routes: routes`
+})
 
 const app = new Vue({
    el: '#app',
+   router,
    data() {
       return {
          info: '',
+         view: true,
          post: [{
                id: 1,
                title: 'My Journey with Vue'
@@ -95,6 +141,7 @@ const app = new Vue({
          .catch(error => {
             console.log(error)
             this.errored = true
+            this.view = false
          })
          .finally(() => this.loading = false)
    },
