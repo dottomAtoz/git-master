4a6815231f008a0e8876c5627be3d9da1b6a7509
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-03-11 18:17:34 +0700

    add++ vue cli

---
+++ src/views/Guide.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <main class="wrapper">
+      <div class="container">
+        <div class="row">
+          <aside class="column column-25 sidebar">
+            <ul class="list__group">
+              <template v-if="view">
+                <li v-for="(i, info) in info" :key="i.id">
+                  <a
+                    class="button button-clear list__group--item"
+                    :href="i.link"
+                  >
+                    {{ i.title }}
+                  </a>
+                </li>
+              </template>
+              <li v-else>Saat ini masih dalam tahap pengembangan</li>
+            </ul>
+          </aside>
+          <article class="column content">
+            <!-- <alert bg="success" bold="Danger" msg="Alert ini untuk pesan sukses"></alert> -->
+            <template v-if="view">
+              {{ info }}
+            </template>
+            <p v-else>Saat ini masih dalam tahap pengembangan</p>
+          </article>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
